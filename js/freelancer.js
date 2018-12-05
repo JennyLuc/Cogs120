1887413dd6c3034c75c643db441e499007850bb7
Jenny Luc <jeluc@ucsd.edu>
2018-12-05 11:21:00 -0800

    checking, added address

--- js/freelancer.js
+++ js/freelancer.js
@@ -126,15 +126,18 @@
   });
 
   // Floating label headings for the contact form
-  $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-      $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-      $(this).removeClass("floating-label-form-group-with-focus");
+    $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
     });
-  });
+    // function myFunction(){
+    //     alert("Page is loaded");
+    // }
 })(jQuery); // End of use strict
 
 
@@ -142,107 +145,119 @@
 
 
 
-$('#save_button').click(createNew);
-//After clicking on id='#save_button', creates and runs function createNew
-function createNew(){
+    $('#save_button').click(createNew);
+    //After clicking on id='#save_button', creates and runs function createNew
+    function createNew(){
 
 
 
-  //creates variable 'name' from 'new_activity_name'(id of the an input)
-    //creates localStorage piece called 'new_activity_name' from 'name'
-    var name = document.getElementById("new_activity_name");
-    localStorage.setItem("new_activity_name", name.value);
+        //creates variable 'name' from 'new_activity_name'(id of the an input)
+        //creates localStorage piece called 'new_activity_name' from 'name'
+        var name = document.getElementById("new_activity_name");
+        localStorage.setItem("new_activity_name", name.value);
 
-    var why = document.getElementById("new_activity_why");
-    localStorage.setItem("new_activity_why", why.value);
+        var why = document.getElementById("new_activity_why");
+        localStorage.setItem("new_activity_why", why.value);
+
+        var day = document.getElementById("day");
+        localStorage.setItem("new_activity_day", day.value);
+
+        var time = document.getElementById("time");
+        localStorage.setItem("new_activity_time", time.value);
+
+        var noon = document.getElementById("noon");
+        localStorage.setItem("new_activity_noon", noon.value);
 
 
 
+        var new_activity = {
+            activity_name: localStorage.getItem('new_activity_name'),
+            activity_why: localStorage.getItem('new_activity_why'),
+            activity_day: localStorage.getItem('new_activity_day'),
+            activity_time: localStorage.getItem('new_activity_time'),
+            activity_noon: localStorage.getItem('new_activity_noon')
+        }
 
 
-    var new_activity = {
-      activity_name: localStorage.getItem('new_activity_name'),
-      activity_why: localStorage.getItem('new_activity_why')
+
+        var activity_list = JSON.parse(localStorage.getItem('list')) || [];
+        // add to it,
+        activity_list.push({
+            activity_name: localStorage.getItem('new_activity_name'),
+            activity_why: localStorage.getItem('new_activity_why'),
+        });
+        // then put it back.
+        localStorage.setItem('list', JSON.stringify(activity_list));
+
+
+
+        // compile the template
+        var source  = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        var parentDiv = $("#templatedProjects");
+
+
+        // start with a simple template
+        var html = template(new_activity);
+        console.log(html);
+        parentDiv.append(html);
     }
 
 
 
-    var activity_list = JSON.parse(localStorage.getItem('list')) || [];
-    // add to it,
-    activity_list.push({
-      activity_name: localStorage.getItem('new_activity_name'),
-      activity_why: localStorage.getItem('new_activity_why'),
-    });
-    // then put it back.
-    localStorage.setItem('list', JSON.stringify(activity_list));
+
+    $(document).ready(function(){
+
+        list = JSON.parse(localStorage.getItem('list'))
+
+        var source  = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        var parentDiv = $("#savedProjects");
+
+
+        for (var i=0; i < list.length, i++;) {
+            var html = template(JSON.parse(localStorage.getItem('list'))[i]);
+            console.log(html);
+            parentDiv.append(html);
+        }
+    })
 
 
 
- // compile the template
- var source  = $("#entry-template").html();
- var template = Handlebars.compile(source);
- var parentDiv = $("#templatedProjects");
+    $('#signup_button').click(createUser);
+    //After clicking on id='#save_button', creates and runs function createNew
+    function createUser(){
+
+        var name = document.getElementById("first_name");
+        localStorage.setItem("first_name", name.value);
+
+        var email = document.getElementById("email");
+        localStorage.setItem("email", email.value);
 
 
-  // start with a simple template
-  var html = template(new_activity);
-  console.log(html);
-  parentDiv.append(html);
-}
+        var user = document.getElementById("username");
+        localStorage.setItem("username", user.value);
 
 
-
-
-$(document).ready(function(){
-
-  list = JSON.parse(localStorage.getItem('list'))
-
-  var source  = $("#entry-template").html();
-  var template = Handlebars.compile(source);
-  var parentDiv = $("#savedProjects");
-
-
-  for (var i=0; i < list.length, i++;) {
-    var html = template(JSON.parse(localStorage.getItem('list'))[i]);
-    console.log(html);
-    parentDiv.append(html);
-  }
-})
+        var password = document.getElementById("password");
+        localStorage.setItem("password", password.value);
 
 
 
+        //combines 'name' and 'why' into new object with properties 'activity_name' & 'activity_why'
+        //to properly add to handlbars template (below)
+        var new_user = {
+            first_name: localStorage.getItem('first_name'),
+            email: localStorage.getItem('email'),
+            user: localStorage.getItem('username'),
+            password: localStorage.getItem('password')
+        }
+    }
+
+    function myFunction(){
+        alert("Page is loaded");
+        alert (list[0].activity_day);
 
 
-
-
-$('#signup_button').click(createUser);
-//After clicking on id='#save_button', creates and runs function createNew
-function createUser(){
-
- var name = document.getElementById("first_name");
-  localStorage.setItem("first_name", name.value);
-
-   var email = document.getElementById("email");
-  localStorage.setItem("email", email.value);
-
-
-   var user = document.getElementById("username");
-  localStorage.setItem("username", user.value);
-
-
-   var password = document.getElementById("password");
-  localStorage.setItem("password", password.value);
-
-
-
-  //combines 'name' and 'why' into new object with properties 'activity_name' & 'activity_why' 
-  //to properly add to handlbars template (below)
-  var new_user = {
-    first_name: localStorage.getItem('first_name'),
-    email: localStorage.getItem('email'),
-    user: localStorage.getItem('username'),
-    password: localStorage.getItem('password')
-  }
-}
-
-
+    }
+// }
